c2895031805b36bf7db0f8ac16f4efeef48603f6
Rusnietis <tskridla@yahoo.com>
2024-09-04 21:59:03 +0300

    Atnaujinta 2024.09.04

--- mok1/src/App.jsx
+++ mok1/src/App.jsx
@@ -1,31 +1,41 @@
 //Lists uzdaviniai
 
 import './App.scss';
+import Bala from './Components/Lists/Pirmuz/Bala';
 // import randomColor from './Functions/randomColor';
 // import Dogs from './Components/Lists/Dogs';
 
 function App() {
 
-    const dogs = ['šuo', 'šunius', 'Bobikas', 'kudlius', 'Šarikas', 'avigalvis'];
-    console.log(dogs);
+    const seaPlaners = [
+        { id: 1, type: 'man', name: 'Lina', color: 'blue' },
+        { id: 2, type: 'car', name: 'Opel', color: 'red' },
+        { id: 3, type: 'animal', name: 'Vilkas', color: 'green' },
+        { id: 4, type: 'fish', name: 'Ungurys', color: 'yellow' },
+        { id: 5, type: 'man', name: 'Tomas', color: 'green' },
+        { id: 6, type: 'animal', name: 'Bebras', color: 'red' },
+        { id: 7, type: 'animal', name: 'Barsukas', color: 'green' },
+        { id: 8, type: 'car', name: 'MB', color: 'blue' },
+        { id: 9, type: 'car', name: 'ZIL', color: 'red' },
+        { id: 10, type: 'man', name: 'Teta Toma', color: 'yellow' },
+    ];
+
+    console.log(seaPlaners);
 
     return (
         <div className="App">
             <header className="App-header">
                 <h1>React lists uždaviniai</h1>
                 <h4>Pirmas uzdavinys</h4>
-                {/* <div className="squares">
+                <div>
                     {
-                        dogs.map((animal, i) => <Dogs key={i} vardas={animal} />)
+                        seaPlaners.map((item) => <Bala key={item.id} vardas={item.name} color={item.color} />)
                     }
-                </div> */}
+                </div>
+
                 <h4>Antras uzdavinys</h4>
-                {/* <div className="circles">
-                    {
-                        dogs.sort().map((animal, i) => <Dogs key={i} skaicius={i + 1} vardas={animal} />)
-                    }
-                </div> */}
-            
+
+
 
 
 
---
+++ mok1/src/Components/Lists/Pirmuz/Bala.jsx
@@ -0,0 +1,13 @@
+export default function Bala({ vardas, color }) {
+
+    return (
+        <>
+            <h3 style={{color: color}}>
+                
+                {vardas}
+            </h3>
+
+     
+        </>
+    )
+};
