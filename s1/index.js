de625b95e272d8913d56874c451d70f1213e960d
Rusnietis <tskridla@yahoo.com>
2024-10-10 21:43:55 +0300

    Atnaujinta 2024.10.10 037A

--- s1/index.js
+++ s1/index.js
@@ -1,11 +1,46 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('node:fs');
 const app = express();
 const port = 3001;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+//app.use(bodyParser.json())
+
+// router
+
 app.get('/', (req, res) => {
-  res.send('<h1>Labas, Meskenai</h1>')
+  console.log('Buvo užklausta /');
+  res.send('Labas Meškėnai!');
+});
+
+app.get('/m/44', (req, res) => {
+  console.log('Color:', req.query.color);
+  res.send(`<h1 style="color:${req.query.color}; font-size:${req.query.size}px">Labas, Meškėnai!</h1>`);
+});
+
+app.get('/bebrenas/:color', (req, res) => {
+  console.log('Color:', req.params.color);
+  res.send(`<h1 style="color:${req.params.color}; font-size:${req.query.size}px">Labas, Bebrėnai!</h1>`);
+});
+
+app.get('/form', (req, res) => {
+  const html = fs.readFileSync('./htmls/form.html', 'utf8');
+  res.send(html);
+})
+
+app.post('/form', (req, res) => {
+  let data = fs.readFileSync('./data/data.json', 'utf8');// nuskaitymas
+  data = JSON.parse(data); //pavertimas i masyva
+  data.push(req.body);
+  data = JSON.stringify(data);
+  fs.writeFileSync('./data/data.json', data);
+
+  res.redirect('/form');
 })
 
 app.listen(port, () => {
-  console.log(`Meskenas klauso ${port} porto.`)
-})
+  console.log(`Meskėnas klauso ${port} porto.`);
+});
