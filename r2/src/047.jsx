9c4005b7a6eb7353dfd5a66e849bc3aa45a59cfa
Rusnietis <tskridla@yahoo.com>
2024-11-20 20:38:42 +0200

    Atnaujinta 2024.11.20 047

---
+++ r2/src/047.jsx
@@ -0,0 +1,26 @@
+
+import './App.scss';
+import './form.scss';
+import './buttons.scss';
+import useLocalstorage from './Hooks/useLocalstorage';
+
+
+export default function App() {
+    const [racoons, setRacoons, meskenai, setMeskenai] = useLocalstorage('racoons', 'meskenai', 0);
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>CUSTOM HOOK</h1>
+                <h2>Now we have {racoons} Racoons</h2>
+                <h2>Dabar mes turim tiek {meskenai} Meskenu</h2>
+                <div className="buttons">
+                    <button className="green" onClick={_=>setRacoons(r => r +1)}>+</button>
+                    <button className="green" onClick={_=>setMeskenai(r => r +1)}>+</button>
+                </div>
+                
+            </header>
+        </div>
+    );
+}
--- r2/src/Hooks/useLocalstorage.jsx
+++ r2/src/Hooks/useLocalstorage.jsx
@@ -1,33 +1,34 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export default function useLocalstorage(key, keyLt, initialValue) {
-    
+
     const [value, setValue] = useState(initialValue);
     const [valueLt, setValueLt] = useState(initialValue);
 
-    // const [value, setValue] = useState(_ => {
+    // const [value, setValue] = useState(_=>{
     //     console.log('useState', 'key', key, 'initialValue', initialValue);
     //     const item = window.localStorage.getItem(key);
     //     return item ? JSON.parse(item) : initialValue;
-    // });
-
+    // })
 
     useEffect(_ => {
+        //console.log('useEffect 1', 'key', key, 'initialValue', initialValue);
         const item = window.localStorage.getItem(key);
         setValue(item ? JSON.parse(item) : initialValue);
         const itemLt = window.localStorage.getItem(keyLt);
-        setValueLt(itemLt ? JSON.parse(itemLt) : initialValue);
-    }, [key, keyLt, initialValue]);
+        setValueLt(item ? JSON.parse(itemLt) : initialValue);
 
+    }, [key, keyLt,initialValue]);
 
     useEffect(_ => {
+        //console.log('useEffect 2', 'key', key, 'value', value);
         window.localStorage.setItem(key, JSON.stringify(value));
     }, [value, key]);
 
     useEffect(_ => {
+        //console.log('useEffect 2', 'key', key, 'value', value);
         window.localStorage.setItem(keyLt, JSON.stringify(valueLt));
     }, [valueLt, keyLt]);
-
-
     return [value, setValue, valueLt, setValueLt];
+
 }
