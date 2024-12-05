9cba9eec7ee2a6b9941ff67c260f4b3b4d9e5ee0
Rusnietis <tskridla@yahoo.com>
2024-12-05 21:20:47 +0200

    Atnaujinta 2024.12.05 Bankas b, bs-login

---
+++ b-login/src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Router, RouterProvider } from './Contexts/Router';
+import './Style/App.scss';
+
+export default function App() {
+  return (
+    <RouterProvider>
+      <div className="App">
+        <header className="App-header">
+          <h1>Bankas express</h1>
+        </header>
+      </div>
+    </RouterProvider>
+  );
+}
+
+
