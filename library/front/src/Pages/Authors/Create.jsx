11bfaefe13e9b9b4a7b9ee7f38bd2a9c91f58fc4
Rusnietis <tskridla@yahoo.com>
2025-04-04 11:39:03 +0300

    Atnaujinta 2025.04.04 library front, back js068 validate

--- library/front/src/Pages/Authors/Create.jsx
+++ library/front/src/Pages/Authors/Create.jsx
@@ -1,5 +1,7 @@
 import { useState, useContext } from 'react';
 import { Authors } from '../../Contexts/Authors';
+import * as v from '../../Validators/textInputs';
+import { MessagesContext } from '../../Contexts/Messages'
 
 const defaultInputs = {
     name: '',
@@ -12,17 +14,33 @@ export default function Create() {
 
     const [inputs, setInputs] = useState(defaultInputs);
 
-    const {setStoreAuthor} = useContext(Authors)
+    const { setStoreAuthor } = useContext(Authors);
+    const {addMessage} = useContext(MessagesContext);
+    const [e, setE] = useState(new Map());
 
     
+
+
     const handlerChange = e => {
         setInputs(prev => ({ ...prev, [e.target.id]: e.target.value })); // 
     }
 
     const create = _ => {
+        const errors = new Map();
+        v.validate(inputs.name, 'name', errors, [v.required, v.string, v.lettersOnly, [v.min, 3], [v.max, 100]]);
+        v.validate(inputs.surname, 'surname', errors, [v.required, v.string, v.lettersOnly, [v.min, 3], [v.max, 100]]);
+        v.validate(inputs.nickname, 'nickname', errors, [v.sometimes, v.string, [v.min, 3], [v.max, 100]]);
+        v.validate(inputs.born, 'born', errors, [v.required, v.date]);
+
+        if (errors.size > 0) {
+            errors.forEach(err => addMessage({type: 'danger', text: err}));
+            setE(errors);
+            return;
+        }
+
+        return;
         setStoreAuthor(inputs);
         setInputs(defaultInputs);
-
     }
 
     return (
@@ -34,19 +52,19 @@ export default function Create() {
 
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" value={inputs.name} onChange={handlerChange} />
+                    <input type="text" className="form-control" style={{borderColor: e.has('name') ? 'crimson' : null }} id="name" value={inputs.name} onChange={handlerChange} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="surname" className="form-label">Surname</label>
-                    <input type="text" className="form-control" id="surname" value={inputs.surname} onChange={handlerChange} />
+                    <input type="text" className="form-control" style={{borderColor: e.has('surname') ? 'crimson' : null }} id="surname" value={inputs.surname} onChange={handlerChange} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="nickname" className="form-label">Nickname</label>
-                    <input type="text" className="form-control" id="nickname" value={inputs.nickname} onChange={handlerChange} />
+                    <input type="text" className="form-control" style={{borderColor: e.has('nickname') ? 'crimson' : null }} id="nickname" value={inputs.nickname} onChange={handlerChange} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="born" className="form-label">Born</label>
-                    <input type="date" className="form-control" id="born" value={inputs.born} onChange={handlerChange} />
+                    <input type="date" className="form-control" style={{borderColor: e.has('born') ? 'crimson' : null }} id="born" value={inputs.born} onChange={handlerChange} />
                 </div>
             </div>
             <div className="card-footer">
---
+++ library/front/src/Validators/textInputs.js
@@ -0,0 +1,89 @@
+
+export const validate = (value, input, errors, rules) => {
+    for (let i = 0; i < rules.length; i++) {
+        let result;
+        if(!Array.isArray(rules[i])) {
+            result = rules[i](value);
+        }else {
+            result = rules[i][0](value, rules[i][1]);
+        }
+      
+        if (result === 1) {
+            return true
+        }
+        if (result === 0) {
+            continue;
+        }
+        if (result !== true) {
+            errors.set(input, input + ': ' + result);
+            return false;
+        }
+    }
+    return true
+}
+
+export const sometimes = value => {
+    if (value) {
+        return 0;
+    }
+    return 1;
+}
+
+export const required = value => {
+    if (value) {
+        return true;
+    }
+    return 'Required';
+}
+
+export const min = (value, min) => {
+    if( typeof value === 'number') {
+        if (value >= min) {
+        return true;
+        }
+        return 'Too small minimum' + min;
+    }
+    if (value.length >= min) {
+        return true;
+    }
+    return 'Too short minimum ' + min + ' characters';
+}
+
+export const max = (value, max) => {
+    if( typeof value === 'number') {
+        if (value <= max) {
+        return true;
+        }
+        return 'Too big maximum' + max;
+    }
+    if (value.length <= max) {
+        return true;
+    }
+    return 'Too long maximum ' + max + ' characters';
+}
+
+export const string = value => {
+    if (typeof value === 'string') {
+        return true;
+    }
+    return 'Not a string';
+}
+
+export const number = value => {
+    if (typeof value === 'number') {
+        return true;
+    }
+    return 'Not a number';
+}
+
+export const lettersOnly = value => {
+    if (value.match(/^[a-zA-Z]+$/)) {
+        return true;
+    }
+    return 'Only Letters';
+}
+
+export const date = value => {
+
+
+}
