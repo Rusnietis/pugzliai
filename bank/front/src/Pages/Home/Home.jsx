03858628957180feaa444f994b2013584a986754
Rusnietis <tskridla@yahoo.com>
2025-07-03 21:38:42 +0300

    Atnaujinta 2025.07.03 Bank Home page

---
+++ bank/front/src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+
+
+
+export default function Homes() {
+    return (
+        <div className="header">
+            <div className="header_contents">
+                <h2>Banko aplikacija v.4</h2>
+                <p>Ši banko aplikacija sukurta naudojant React, Express ir MariaDB. Ji leidžia vartotojams kurti sąskaitas, pridėti nuotraukas, peržiūrėti balansą ir atlikti kitas operacijas.</p>
+            </div>
+        </div>
+    )
+}
--- bank/front/src/Pages/Home/Layout.jsx
+++ bank/front/src/Pages/Home/Layout.jsx
@@ -1,28 +1,33 @@
 import Nav from '../../Components/Nav';
 import { useContext } from 'react';
-import { Home } from '../../Contexts/Home';
+import Homes from './Home';
 
 
 
 export default function Layout() {
 
-    const { home } = useContext(Home)
+    //const { home } = useContext(Home)
 
     //if (!home) return (< div className="loader"><div></div></div>)
 
     return (
         <>
             <Nav />
-            <h1>Pagrindinis</h1>
+            <Homes />
+            {/* <h1>Pagrindinis</h1>
             <div className="header">
-                {/* <img src="/pinigai.jpg" alt="Pinigai"/> */}
-                <div className="header_content">
+
+                <div style={{
+                    backgroundImage: `url(${pinigai})`,
+                    height: '300px',
+                    backgroundSize: 'cover',
+                }}>
                     <h2>Banko sistema</h2>
                     <p>Ši banko sistema sukurta naudojant React, Express ir MongoDB. Ji leidžia vartotojams kurti sąskaitas, peržiūrėti balansą ir atlikti operacijas.</p>
 
                 </div>
-               
-            </div>
+
+            </div> */}
 
         </>
     );
