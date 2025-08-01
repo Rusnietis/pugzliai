ae7bef10dc12f7c31f650afdefc0e4b41987c642
Rusnietis <tskridla@yahoo.com>
2025-08-01 21:30:35 +0300

    Atnaujinta 2025.08.01 Bank

--- bank/front/src/Pages/Home/Home.jsx
+++ bank/front/src/Pages/Home/Home.jsx
@@ -1,10 +1,17 @@
 
 
 
-export default function Homes() {
+export default function Home() {
     return (
-        <div className="header">
-            <div className="header_contents">
+        <div className="header"
+            style={{
+                backgroundImage: 'url(/pinigai1.jpeg)',
+                backgroundSize: 'cover',
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                position: 'relative'
+            }}>
+            <div className="header_contents ">
                 <h2>Banko aplikacija v.4</h2>
                 <p>Ši banko aplikacija sukurta naudojant React, Express ir MariaDB. Ji leidžia vartotojams kurti sąskaitas, pridėti nuotraukas, peržiūrėti balansą ir atlikti kitas operacijas.</p>
             </div>
