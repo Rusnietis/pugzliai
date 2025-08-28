bd269efeea05930e1b2bccd1fdf0f577b5781808
Rusnietis <tskridla@yahoo.com>
2025-08-28 10:05:29 +0300

    Atnaujinta 2025.08.28 Bank front, back Stats

--- bank/front/src/Pages/Customers/Header.jsx
+++ bank/front/src/Pages/Customers/Header.jsx
@@ -39,6 +39,7 @@ export default function Header() {
                             </div>
                         )
                     }
+                    
                     <hr />
               
                 </div>
